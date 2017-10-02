cc461d843fa10670ef364509039c8be4f766e043
edwinmejia-123 <30848466+edwinmejia-123@users.noreply.github.com>
2017-10-01 22:42:18 -0600

    Update registro.js

--- js/registro.js
+++ js/registro.js
@@ -26,6 +26,48 @@ var Login = {
 		form.txtNumero.value="";
 		form.txtNaciemiento.value="";
 		return datos;
+	},
+	Ingresar:function(datos,form)
+	{
+		if (datos.nombre_us==form.txtUsuario.value && datos.contra==form.txtContra.value)
+		{
+			this.contador+=1;
+			alert(datos.preguntas[0]);
+			if (this.contador==1)
+			{
+				var cuenta="";
+				cuenta+="<!DOCTYPE html>\n";
+				cuenta+="<html lang=\"es\">\n";
+				cuenta+="<head>\n";
+				cuenta+="<title>Cuentas Bacaria</title>"
+				cuenta+="<link rel='stylesheet' type='text/css' href='../css/cuenta.css'>\n";
+				cuenta+="<meta charset='utf-8'>\n";
+				cuenta+="</head>\n";
+				cuenta+="<body>\n";
+				cuenta+="<h1>Datos de Cuenta Bancaria</h1>\n";
+				cuenta+="<form name='frmcuentas'>\n";
+				cuenta+="<fieldset>\n";
+				cuenta+="<label>Efectivo</label>\n";
+				cuenta+="<input type='text' name='efectivo' id='efectivo'>\n";
+				cuenta+="</fieldset>\n";
+				cuenta+="<fieldset>\n";
+				cuenta+="<legend>Cuentas bancarias</legend>\n";
+				cuenta+="<label>Banco</label>\n";
+				cuenta+="<input type='text' name='Banco'>\n";
+				cuenta+="<label>Numero de cuenta</label>\n";
+				cuenta+="<input type='text' name='numero_cuenta'>\n";
+				cuenta+="<label>Saldo Actual</label>\n";
+				cuenta+="<input type='text' name='saldo'>\n";
+				cuenta+="</fieldset>\n";
+				cuenta+="<input type='button' name='btnIngresar' value='Ingresar'>\n";
+				cuenta+="</form>\n";
+				cuenta+="</body>\n";
+				cuenta+="</html>\n";
+				document.write(cuenta);
+			}
+		}else{
+			alert("Datos ingresados incorrectos");
+		}
 	}
 
 
@@ -60,6 +102,12 @@ function iniciar(){
 	 			alert("No ingreso correctamente los datos");
 	 		}
 		});
+	}
+	//inicio de secion
+	if(btnIngresar.addEventListener){
+		btnIngresar.addEventListener("click",function(){
+			Login.Ingresar(datos,document.frmLoign);
+		},false);
 	}
 
 }
